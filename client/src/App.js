b56cdd2d8e7eaf99ff1c796f2e271d88e531babf
Jana Rajakumar <jana.stc@gmail.com>
2018-10-24 20:14:06 -0400

    Add material-ui package

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Button from '@material-ui/core/Button';
+import { AccessAlarm } from '@material-ui/icons';
 
 
 class App extends Component {
   render() {
     return (
       <div>
+        <CssBaseline/>
         <h1>HELLO</h1>
+        <AccessAlarm/>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
       </div>
     );
   }
 }
 
-export default App;
+export default App
