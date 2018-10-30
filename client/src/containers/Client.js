df5db38d5862d422db4d984812a4b18860dcd696
Jana Rajakumar <jana.stc@gmail.com>
2018-10-29 23:13:18 -0400

    Connect client table list with redux

--- client/src/containers/Client.js
+++ client/src/containers/Client.js
@@ -1,192 +1,67 @@
 import React, { Component } from 'react'
 import Table from 'components/Table'
+import { connect } from 'react-redux'
+import { format } from 'date-fns'
+
+import { getUsers } from 'actions/users'
 
 
 const rows = [
-  { field: 'username', numeric: false, label: 'Username' },
-  { field: 'email', numeric: false, label: 'Email' },
+  { field: 'stb_mac', numeric: false, label: 'MAC' },
+  { field: 'full_name', numeric: false, label: 'Full Name' },
   // { field: 'firstName', numeric: false, label: 'First Name' },
   // { field: 'lastName', numeric: false, label: 'Last Name' },
-  { field: 'phoneNo', numeric: false, label: 'Telephone' },
-  { field: 'accountStatus', numeric: false, label: 'Account Status' },
+  { field: 'phone', numeric: false, label: 'Telephone' },
+  { field: 'tariff_expired_date', numeric: false, label: 'Expiry Date' },
   // { field: 'parentID', numeric: false, label: 'Parent Username' },
   // { field: 'childrenCount', numeric: true, label: 'No of Children' },
-  { field: 'creditsAvailable', numeric: true, label: 'Credits Available' },
-  { field: 'creditsOnHold', numeric: true, label: 'Credits on Hold' },
-  { field: 'createdAt', numeric: false, label: 'Created At' },
+  { field: 'last_active', numeric: false, label: 'Last Active' },
   // { field: 'updatedAt', numeric: false, label: 'Updated At' }
 ]
 
 
-const data = [
-  {
-    username: 'test',
-    email: 'test@mail',
-    phoneNo: '64786543',
-    accountStatus: 'active',
-    creditsAvailable: 15,
-    creditsOnHold: 10,
-    createdAt: 'some date'
-  },
-  {
-    username: 'test2',
-    email: 'test@maile',
-    phoneNo: '4561384',
-    accountStatus: 'disabled',
-    creditsAvailable: 12,
-    creditsOnHold: 6,
-    createdAt: 'some datesa'
-  },
-  {
-    username: 'test3',
-    email: 'test@m131ail',
-    phoneNo: '15618461',
-    accountStatus: 'active',
-    creditsAvailable: 2,
-    creditsOnHold: 1,
-    createdAt: 'some dateasd'
-  },
-  {
-    username: 'test2',
-    email: 'test@maile',
-    phoneNo: '4561384',
-    accountStatus: 'disabled',
-    creditsAvailable: 12,
-    creditsOnHold: 6,
-    createdAt: 'some datesa'
-  },
-  {
-    username: 'test2',
-    email: 'test@maile',
-    phoneNo: '4561384',
-    accountStatus: 'disabled',
-    creditsAvailable: 12,
-    creditsOnHold: 6,
-    createdAt: 'some datesa'
-  },
-  {
-    username: 'test2',
-    email: 'test@maile',
-    phoneNo: '4561384',
-    accountStatus: 'disabled',
-    creditsAvailable: 12,
-    creditsOnHold: 6,
-    createdAt: 'some datesa'
-  },
-  {
-    username: 'test2',
-    email: 'test@maile',
-    phoneNo: '4561384',
-    accountStatus: 'disabled',
-    creditsAvailable: 12,
-    creditsOnHold: 6,
-    createdAt: 'some datesa'
-  },
-  {
-    username: 'test2',
-    email: 'test@maile',
-    phoneNo: '4561384',
-    accountStatus: 'disabled',
-    creditsAvailable: 12,
-    creditsOnHold: 6,
-    createdAt: 'some datesa'
-  },
-  {
-    username: 'test2',
-    email: 'test@maile',
-    phoneNo: '4561384',
-    accountStatus: 'disabled',
-    creditsAvailable: 12,
-    creditsOnHold: 6,
-    createdAt: 'some datesa'
-  },
-  {
-    username: 'test2',
-    email: 'test@maile',
-    phoneNo: '4561384',
-    accountStatus: 'disabled',
-    creditsAvailable: 12,
-    creditsOnHold: 6,
-    createdAt: 'some datesa'
-  },
-  {
-    username: 'test2',
-    email: 'test@maile',
-    phoneNo: '4561384',
-    accountStatus: 'disabled',
-    creditsAvailable: 12,
-    creditsOnHold: 6,
-    createdAt: 'some datesa'
-  },
-  {
-    username: 'test2',
-    email: 'test@maile',
-    phoneNo: '4561384',
-    accountStatus: 'disabled',
-    creditsAvailable: 12,
-    creditsOnHold: 6,
-    createdAt: 'some datesa'
-  },
-  {
-    username: 'test2',
-    email: 'test@maile',
-    phoneNo: '4561384',
-    accountStatus: 'disabled',
-    creditsAvailable: 12,
-    creditsOnHold: 6,
-    createdAt: 'some datesa'
-  },
-  {
-    username: 'test2',
-    email: 'test@maile',
-    phoneNo: '4561384',
-    accountStatus: 'disabled',
-    creditsAvailable: 12,
-    creditsOnHold: 6,
-    createdAt: 'some datesa'
-  },
-  {
-    username: 'test2',
-    email: 'test@maile',
-    phoneNo: '4561384',
-    accountStatus: 'disabled',
-    creditsAvailable: 12,
-    creditsOnHold: 6,
-    createdAt: 'some datesa'
-  },
-  {
-    username: 'test2',
-    email: 'test@maile',
-    phoneNo: '4561384',
-    accountStatus: 'disabled',
-    creditsAvailable: 12,
-    creditsOnHold: 6,
-    createdAt: 'some datesa'
-  },
-  {
-    username: 'test2',
-    email: 'test@maile',
-    phoneNo: '4561384',
-    accountStatus: 'disabled',
-    creditsAvailable: 12,
-    creditsOnHold: 6,
-    createdAt: 'some datesa'
+class Client extends Component {
+
+  componentDidMount = () => {
+    this.props.getUsers()
   }
-]
 
+  getTableData = () => {    
+    let displayData = []
+    for (let client of this.props.clients) {
+      let clientData = {}
+      for (let row of rows) {
+        if (['tariff_expired_date', 'last_active'].includes(row.field)) {
+          clientData[row.field] = format(Date.parse(client[row.field]), 'd MMMM YYYY @ HH:mm:ss')
+        } else {
+          clientData[row.field] = client[row.field]
+        }
+      }
+      displayData.push({...clientData})
+    }
+    return displayData
+  } 
 
-export default class Client extends Component {
   render() {
     return (
-      <div>
-        <Table
-          title='Clients'
-          rows={rows}
-          data={data}
-          orderBy='creditsAvailable'
-          mobileView={false}
-        />
-      </div>
+      <Table
+        title={'Clients'}
+        rows={rows}
+        data={this.getTableData()}
+        orderBy='tariff_expired_date'
+        mobileView={this.props.mobileView}
+      />
     )
   }
 }
+
+
+const mapStateToProps = state => ({
+  clients: state.users.clients,
+})
+
+const mapDispatchToProps = dispatch => ({
+  getUsers: () => dispatch(getUsers()),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Client)
