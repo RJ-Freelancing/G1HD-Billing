f46d16eb8dcfcfba07ec8acbe54d88f4c8e809e6
yrameshk26 <yrameshk26@gmail.com>
2018-11-11 01:28:33 -0500

    Added date-fns package, and finished transaction with expiry date change

--- server/controllers/transactionController.js
+++ server/controllers/transactionController.js
@@ -3,7 +3,7 @@ import userRepo from '../models/userModel'
 import clientRepo from '../models/clientModel'
 import { checkPermissionRights } from '../_helpers/checkPermission'
 import axios from 'axios'
-import querystring from 'querystring'
+import dateFns from 'date-fns'
 
 const ministraAPI = process.env.MINISTRA_HOST+'stalker_portal/api/'
 const ministaUser = process.env.MINISTRA_USER
@@ -50,9 +50,13 @@ export async function addTransaction(req, res, next) {
     await req.user.update({creditsOnHold : (req.user.creditsOnHold-credits)} )
   }
   if (req.user.userType == "reseller"){
+    const client = await clientRepo.findOne({clientMac : transactionTo})
+    if (credits < 0 && client.accountBalance<(-1*credits)) return res.status(400).json("Not enough balance to recover the credits. Try again with lesser credits.")
     await clientRepo.findOneAndUpdate({clientMac : transactionTo}, { $inc: { accountBalance : credits }})
   }
   else {
+    const user = await userRepo.findOne({username : transactionTo})
+    if (credits < 0 && user.creditsAvailable<(-1*credits)) return res.status(400).json("Not enough balance to recover the credits. Try again with lesser credits.")
     await userRepo.findOneAndUpdate({username : transactionTo}, { $inc : { creditsAvailable : credits }})
   }
   await axios.get(ministraAPI+'accounts/'+transactionTo, config)
@@ -62,29 +66,33 @@ export async function addTransaction(req, res, next) {
   .catch(error => {
     console.log("Ministra API Error : " + error)
   })
-  if (res.locals.clientExpiryDate == null){
-    const expiredDate = `tariff_expired_date=${dateAfterNthMonth(1)}`
-    await axios.put(ministraAPI+'accounts/'+transactionTo,
-    expiredDate, config) 
-      .then(response => {
-        res.locals.updatedUser = response.data
-      })
-      .catch(error => {
-        console.log("Ministra API Error : " + error)
-      })
+  let expiredDate = []
+  if (res.locals.clientExpiryDate == null || res.locals.clientExpiryDate == "0000-00-00 00:00:00"){
+    expiredDate = await `tariff_expired_date=${expiryDateAfterTransaction(0,credits)}`
   }
+  else {
+    expiredDate = await `tariff_expired_date=${expiryDateAfterTransaction(res.locals.clientExpiryDate, credits)}`
+  }
+  await axios.put(ministraAPI+'accounts/'+transactionTo,
+  expiredDate, config) 
+    .then(response => {
+      res.locals.updatedUser = response.data
+    })
+    .catch(error => {
+      console.log("Ministra API Error : " + error)
+    })
   const transaction = await transactionRepo.create([{ credits, description, transactionFrom, transactionTo }], {lean:true})
   return res.status(201).json({transaction})
 }
 
-function dateAfterNthMonth(n) {
-  var d = new Date(),
-      month = '' + (d.getMonth() + 1+n),
-      day = '' + d.getDate(),
-      year = d.getFullYear();
-
-  if (month.length < 2) month = '0' + month;
-  if (day.length < 2) day = '0' + day;
-
-  return [year, month, day].join('-');
+function expiryDateAfterTransaction(date, n) {
+  var resDate
+  if (date == 0){
+    resDate = new Date()
+  }
+  else {
+    resDate = new Date(date)
+  }
+  resDate = dateFns.addMonths(resDate, n)
+  return dateFns.format(resDate, 'YYYY-MM-DD')
 }
