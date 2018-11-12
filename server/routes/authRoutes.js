b9b15403dd5ae969fef1db3f943a4bb4123d8e2a
yrameshk26 <yrameshk26@gmail.com>
2018-11-12 01:23:31 -0500

    Added routes for refreshtoken

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,13 +4,19 @@ import { schemas } from '../validations/userValidation'
 import { login } from '../controllers/userController'
 
 const passportSignIn = passport.authenticate('local', { session: false })
-
+const passportJWT = passport.authenticate('jwt', { session: false })
 const router = require('express-promise-router')()
 
 router.route('/login')
   .post(
     validateBody(schemas.userLoginSchema),
     passportSignIn,
+    login
+  )
+
+router.route('/refreshtoken')
+  .get(
+    passportJWT,
     login
   )
 
