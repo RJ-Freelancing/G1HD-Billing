044bab4e70ff339388d15f49f4bf31efb9ee4905
yrameshk26 <yrameshk26@gmail.com>
2018-11-13 01:05:28 -0500

    Handled huge calls with macs on ministra

--- server/controllers/ministraController.js
+++ server/controllers/ministraController.js
@@ -32,15 +32,25 @@ export async function getTariffs(req, res, next) {
 export async function sendMsg(req, res, next) {
   const ministraPayLoad = querystring.stringify(req.value.body)
   const mac = req.value.body.ids
-  await ministraPostCalls('stb_msg/', ministraPayLoad, mac, res)
+  var resultMap = await ministraPostCalls('stb_msg/', ministraPayLoad, mac, res)
+  if((resultMap.error)) return res.status(404).json(resultMap.error)
+  return res.status(201).json(resultMap.success)
 }
 
 export async function sendEvent(req, res, next) {
+  let resultMap = {}
   const ministraPayLoad = querystring.stringify(req.value.body)
   var mac = req.value.body.ids
   if (mac == "" && req.user.userType !== "super-admin") return res.status(403).json("You Have No Rights To Perform This Action.")
   if (mac == undefined) return res.status(404).json("Mac Ids are missing...")
-  await ministraPostCalls('send_event/', ministraPayLoad, mac, res)
+  if (mac.length > 30) {
+    resultMap = await hugeMinistaPostCalls('send_event/', ministraPayLoad, mac, res)
+  }
+  else {
+    resultMap = await ministraPostCalls('send_event/', ministraPayLoad, mac, res)
+  }
+  if(resultMap.error) return res.status(404).json(resultMap.error)
+  return res.status(201).json(resultMap.success)
 }
 
 export async function getAccountSub(req, res, next) {
@@ -52,7 +62,9 @@ export async function postAccountSub(req, res, next) {
   const payloadMap = req.value.body.subscribed.map(x => `subscribed[]=${x}&`).join('')
   const ministraPayLoad = payloadMap.substring(0, payloadMap.length - 1)
   const mac = req.params.id
-  await ministraPostCalls('account_subscription/', ministraPayLoad, mac, res)
+  var resultMap = await  ministraPostCalls('account_subscription/', ministraPayLoad, mac, res)
+  if((resultMap.error)) return res.status(404).json((resultMap.error))
+  return res.status(201).json(resultMap.success)
 }
 
 export async function putAccountSub(req, res, next) {
@@ -78,49 +90,44 @@ async function ministraGetCalls(attribute, res) {
     })
 }
 
-// async function ministraPostCalls(attribute, ministraPayLoad, mac, res) {
-//   await axios.post(ministraAPI + attribute + mac,
-//     ministraPayLoad, config)
-//     .then(response => {
-//       if (response.data.status !== 'OK') return res.status(404).json(response.data.error)
-//       return res.status(201).json(response.data.results)
-//     })
-//     .catch(error => {
-//       console.log("Ministra API Error : " + error)
-//       return res.status(404).json(error)
-//     })
-// }
+async function ministraPostCalls(attribute, ministraPayLoad, id, res) {
+  let resultMap = {}
+  await axios.post(ministraAPI + attribute + id,
+    ministraPayLoad, config)
+    .then(response => {
+      res.locals.postResults = response.data
+    })
+    .catch(error => {
+      console.log("Ministra API Error : " + error)
+      resultMap["error"] = error
+    })
+    if (res.locals.postResults.status !== 'OK') resultMap["error"] = res.locals.postResults.error
+    resultMap["success"] = res.locals.postResults.results
+    return resultMap
+}
 
-async function ministraPostCalls(attribute, ministraPayLoad, mac, res) {
-  var results = []
-  if(mac.length > 1){
-    await mac.forEach(element => {
-       axios.post(ministraAPI + attribute + element,
-        ministraPayLoad, config)
-        .then(response => {
-          console.log("element : " + element + ". results : " + results)
-          if (response.data.status !== 'OK') return res.status(404).json(response.data.error)
-          results.push(response.data.results)
-        })
-        .catch(error => {
-          console.log("Ministra API Error : " + error)
-          return res.status(404).json(error)
-        })
-    });
+async function hugeMinistaPostCalls(attribute, ministraPayLoad, ids, res){
+  let chunkedArr = await chunkArray(ids, 30)
+  let responseList = []
+  let resultMap = {}
+  await asyncForEach(chunkedArr, async (element) => {
+    var result = await ministraPostCalls(attribute, ministraPayLoad, element, res)
+    responseList.push(result)
+  })
+  let errorList = []
+  responseList.forEach( (element) => {
+    if(element.error) errorList.push(element.error)
+  })
+  if(errorList.length == responseList.length) {
+    resultMap["error"] = "All of the events failed"
+  }
+  else if(errorList.length == 0) {
+    resultMap["success"] = true
   }
   else {
-    await axios.post(ministraAPI + attribute + mac,
-      ministraPayLoad, config)
-      .then(response => {
-        if (response.data.status !== 'OK') return res.status(404).json(response.data.error)
-        return res.status(201).json(response.data.results)
-      })
-      .catch(error => {
-        console.log("Ministra API Error : " + error)
-        return res.status(404).json(error)
-    })
+    resultMap["error"] = "Few of the events failed." 
   }
-  return res.status(201).json(results)
+  return resultMap
 }
 
 async function ministraPutCalls(attribute, ministraPayLoad, mac, res) {
@@ -146,4 +153,20 @@ async function ministraDeleteCalls(attribute, mac, res) {
       console.log("Ministra API Error : " + error)
       return res.status(404).json(error)
     })
+}
+
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
+function chunkArray(myArray, chunk_size) {
+  let results = [];
+  
+  while (myArray.length) {
+      results.push(myArray.splice(0, chunk_size))
+  }
+
+  return results;
 }
