56143a4ab726c46ac1927c53c75482fd3893e653
Jana Rajakumar <jana.stc@gmail.com>
2018-11-18 13:09:56 -0500

    Remove material ui datefns utils

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from 'config/store'
-import DateFnsUtils from 'material-ui-pickers/utils/date-fns-utils';
-import { MuiPickersUtilsProvider } from 'material-ui-pickers';
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Loading from 'components/Loading';
 import { BrowserRouter } from 'react-router-dom'
@@ -16,12 +14,10 @@ window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={<Loading />} persistor={persistor}>
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <CssBaseline />
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </MuiPickersUtilsProvider>
     </PersistGate>
   </Provider>,
   document.getElementById('root')
