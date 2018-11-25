91aa971fbdc04804129dd89b40576081de653e7a
Jana Rajakumar <jana.stc@gmail.com>
2018-11-24 23:04:02 -0500

    Add ability to change internal user's own password

--- client/src/containers/Profile.js
+++ client/src/containers/Profile.js
@@ -9,6 +9,7 @@ import TextField from '@material-ui/core/TextField'
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
+import Confirmation from 'components/Confirmation'
 
 import { updateProfile } from 'actions/users'
 
@@ -53,31 +54,45 @@ class Profile extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      ...props.auth
+      auth: {...props.auth},
+      newPassword: "",
+      confirmation: false,
     }
   }
 
   componentDidUpdate = (prevProps, prevState, snapshot) => {
     if (!isEqual(prevProps.auth, this.props.auth)) {
-      this.setState({...this.props.auth})
+      this.setState({auth: {...this.props.auth}})
     }
   }
 
   handleTextChange = (field, value) => {
-    this.setState({[field]: value})
+    this.setState({auth: {...this.state.auth, [field]: value}})
   }
 
   updateProfile = (event) => {
     event.preventDefault()
-    const {username, email, firstName, lastName, phoneNo} = this.state
-    this.props.updateProfile(username, {email, firstName, lastName, phoneNo})
+    const {username, email, firstName, lastName, phoneNo} = this.state.auth
+    if (this.state.newPassword){
+      this.confirmationProceed = () => {
+        this.setState({confirmation: false}, ()=>{
+          this.props.updateProfile(username, {email, password: this.state.newPassword, firstName, lastName, phoneNo})
+        })
+      }
+      this.setState({confirmation: true})
+    } else {
+      this.props.updateProfile(username, {email, firstName, lastName, phoneNo})
+    }
   }
 
   checkValidation = () => {
-    const usernameEmpty = this.state.username==="";
-    const phoneNoInvalid = this.state.phoneNo && !this.state.phoneNo.match(validPhoneNo);
+    const usernameEmpty = this.state.auth.username==="";
+    const phoneNoInvalid = this.state.auth.phoneNo && !this.state.auth.phoneNo.match(validPhoneNo);
     return usernameEmpty || phoneNoInvalid
   }
+
+  confirmationProceed = () => this.setState({confirmation: false})
+  confirmationCancel = () => this.setState({confirmation: false})
 
   render() {
     return (
@@ -92,13 +107,13 @@ class Profile extends Component {
               <TextField
                 label="Email"
                 type="email"
-                value={this.state.email}
+                value={this.state.auth.email}
                 onChange={(e)=>this.handleTextChange('email', e.target.value)}
                 fullWidth
                 disabled={this.props.loading}
               />
               <InputMask mask="999-999-9999" 
-                value={this.state.phoneNo}  
+                value={this.state.auth.phoneNo}  
                 onChange={(e)=>this.handleTextChange('phoneNo', e.target.value)}
               >
                 {(inputProps) => (
@@ -107,22 +122,31 @@ class Profile extends Component {
                     label="Phone"
                     fullWidth
                     disabled={this.props.loading}
-                    error={Boolean(this.state.phoneNo) && !this.state.phoneNo.match(validPhoneNo)}
-                    helperText={this.state.phoneNo && !this.state.phoneNo.match(validPhoneNo) ? "Invalid Phone" : null}
+                    error={Boolean(this.state.auth.phoneNo) && !this.state.auth.phoneNo.match(validPhoneNo)}
+                    helperText={this.state.auth.phoneNo && !this.state.auth.phoneNo.match(validPhoneNo) ? "Invalid Phone" : null}
                   />
                 )}
               </InputMask>
               <TextField
                 label="First Name"
-                value={this.state.firstName}
+                value={this.state.auth.firstName}
                 onChange={(e)=>this.handleTextChange('firstName', e.target.value)}
                 fullWidth
                 disabled={this.props.loading}
               />
               <TextField
                 label="Last Name"
-                value={this.state.lastName}
+                value={this.state.auth.lastName}
                 onChange={(e)=>this.handleTextChange('lastName', e.target.value)}
+                fullWidth
+                disabled={this.props.loading}
+              />
+              <TextField
+                label="Change Password"
+                type="password"
+                placeholder="Leave empty to keep the current password unchanged"
+                value={this.state.newPassword}
+                onChange={(e)=>this.setState({newPassword: e.target.value})}
                 fullWidth
                 disabled={this.props.loading}
               />
@@ -132,7 +156,7 @@ class Profile extends Component {
               variant="contained" 
               type="submit" 
               color="primary" 
-              disabled={this.props.loading || this.checkValidation() || isEqual(this.props.auth, this.state)}
+              disabled={(!this.state.newPassword) && (this.props.loading || this.checkValidation() || isEqual(this.props.auth, this.state.auth))}
             >
               Update
               <SaveIcon style={{marginLeft: 5}}/>
@@ -145,21 +169,29 @@ class Profile extends Component {
           <br/><br/>
           <ProfileDetails>
             <Typography variant="subtitle2">Account Type</Typography>
-            <Typography variant="body2">{this.state.userType}</Typography>
+            <Typography variant="body2">{this.state.auth.userType}</Typography>
             <Typography variant="subtitle2">Credits Available</Typography>
-            <Typography variant="body2">{this.state.creditsAvailable}</Typography>
+            <Typography variant="body2">{this.state.auth.creditsAvailable}</Typography>
             {this.props.auth.userType==='reseller' && 
               <>
                 <Typography variant="subtitle2">Credits on Hold</Typography>
-                <Typography variant="body2">{this.state.creditsOwed}</Typography>
+                <Typography variant="body2">{this.state.auth.creditsOwed}</Typography>
               </>
             }
             <Typography variant="subtitle2">Date Joined</Typography>
-            <Typography variant="body2">{format(Date.parse(this.state.createdAt), 'd MMM YYYY @ HH:mm:ss')}</Typography>
+            <Typography variant="body2">{format(Date.parse(this.state.auth.createdAt), 'd MMM YYYY @ HH:mm:ss')}</Typography>
             <Typography variant="subtitle2">Last Updated</Typography>
-            <Typography variant="body2">{format(Date.parse(this.state.updatedAt), 'd MMM YYYY @ HH:mm:ss')}</Typography>
+            <Typography variant="body2">{format(Date.parse(this.state.auth.updatedAt), 'd MMM YYYY @ HH:mm:ss')}</Typography>
           </ProfileDetails>
         </ProfileDetailsWrapper>
+
+      <Confirmation
+        open={this.state.confirmation}
+        message="You are changing your password. Are you sure you want to continue ?"
+        confirmationProceed={this.confirmationProceed}
+        confirmationCancel={this.confirmationCancel}
+        disabled={!this.state.newPassword}
+      />
       </Wrapper>
     )
   }
