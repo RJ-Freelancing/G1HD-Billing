1ed3cf8de58ae6f03a9b614f9179769d8e5dc207
Jana Rajakumar <jana.stc@gmail.com>
2018-12-02 12:57:33 -0500

    Show confirmation dialog when adding +1 credits for clients

--- client/src/containers/ClientList.js
+++ client/src/containers/ClientList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Table from 'components/Table'
 import { connect } from 'react-redux'
+import Confirmation from 'components/Confirmation'
+
 
 import { updateCredits } from 'actions/transactions'
 
@@ -15,11 +17,19 @@ const rows = [
   { field: 'parentUsername', label: 'Reseller', type: 'string'  },
   // { field: 'comment', label: 'Comments', type: 'string'  },
   { field: 'now_playing_content', label: 'Now Playing', type: 'string'  },
-  { field: 'status', label: 'Account Status', type: 'boolean'  }
+  // { field: 'status', label: 'Account Status', type: 'boolean'  }
 ]
 
 
 class ClientList extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      confirmation: false,
+      plusOneClient: null
+    }
+  }
 
   getTableData = () => {    
     let displayData = []
@@ -36,31 +46,47 @@ class ClientList extends Component {
     return displayData
   } 
 
+  confirmationProceed = () => this.setState({confirmation: false})
+  confirmationCancel = () => this.setState({confirmation: false})
+
   incrementClientCredit = (client) => {
-    this.props.updateCredits({
-      credits: 1,
-      description: 'Added 1 credit',
-      transactionTo: client.stb_mac
-    }, client.accountBalance)
+    this.confirmationProceed = () => {
+      this.setState({confirmation: false}, ()=>{
+        this.props.updateCredits({
+          credits: 1,
+          description: 'Added 1 credit',
+          transactionTo: client.stb_mac
+        }, client.accountBalance)
+      })
+    }
+    this.setState({confirmation: true, plusOneClient: client.stb_mac})
   }
 
   render() {
     return (
-      <Table
-        title={'Clients'}
-        rows={rows}
-        data={this.getTableData()}
-        orderBy='tariff_expired_date'
-        orderByDirection='desc'
-        mobileView={this.props.mobileView}
-        gotoLink={(client)=>this.props.history.push(`/clients/${client.stb_mac}`)}
-        addNew={()=>this.props.history.push('/clients/new')}
-        incrementClientCredit={this.props.authUserType==='reseller' ? (client)=>this.incrementClientCredit(client) : false}
-        authCreditsAvailable={this.props.authCreditsAvailable}
-        authcreditsOwed={this.props.authcreditsOwed}
-        tableHeight={this.props.mobileView ? '75vh' : '85vh'}
-        canAdd={this.props.authUserType==='reseller'}
-      />
+      <>
+        <Table
+          title={'Clients'}
+          rows={rows}
+          data={this.getTableData()}
+          orderBy='tariff_expired_date'
+          orderByDirection='desc'
+          mobileView={this.props.mobileView}
+          gotoLink={(client)=>this.props.history.push(`/clients/${client.stb_mac}`)}
+          addNew={()=>this.props.history.push('/clients/new')}
+          incrementClientCredit={this.props.authUserType==='reseller' ? (client)=>this.incrementClientCredit(client) : false}
+          authCreditsAvailable={this.props.authCreditsAvailable}
+          authcreditsOwed={this.props.authcreditsOwed}
+          tableHeight={this.props.mobileView ? '75vh' : '85vh'}
+          canAdd={this.props.authUserType==='reseller'}
+        />
+        <Confirmation
+          open={this.state.confirmation}
+          message={`Are you sure you want to add +1 credits to ${this.state.plusOneClient}`}
+          confirmationProceed={this.confirmationProceed}
+          confirmationCancel={this.confirmationCancel}
+        />
+      </>
     )
   }
 }
